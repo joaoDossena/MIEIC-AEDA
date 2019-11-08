66fa85ee0e9b0d2dc5d7409a925be07df559434d
João Dossena <joaopdossena@gmail.com>
2019-11-08 14:27:34 +0000

    Made build-dir

---
+++ TP6/build-dir/lib/googletest-master/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/joao/Desktop/FEUP/AEDA/TP6/lib/googletest-master
+# Build directory: /home/joao/Desktop/FEUP/AEDA/TP6/build-dir/lib/googletest-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
