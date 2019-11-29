f61e0b1d4b53f212db909bd934f2ff7649023eb0
João Dossena <joaopdossena@gmail.com>
2019-11-29 15:16:12 +0000

    TP8 added but TP7 still not done

---
+++ TP8/build-dir/CMakeFiles/aeda1920_fp09.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aeda1920_fp09.dir/Tests/aposta.cpp.o"
+  "CMakeFiles/aeda1920_fp09.dir/Tests/jogador.cpp.o"
+  "CMakeFiles/aeda1920_fp09.dir/Tests/tests.cpp.o"
+  "CMakeFiles/aeda1920_fp09.dir/main.cpp.o"
+  "aeda1920_fp09"
+  "aeda1920_fp09.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aeda1920_fp09.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TP8/build-dir/lib/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/joao/Desktop/FEUP/AEDA/TP8/lib/googletest-master/googlemock
+# Build directory: /home/joao/Desktop/FEUP/AEDA/TP8/build-dir/lib/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
