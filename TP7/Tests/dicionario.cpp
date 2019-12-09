13994b726cf7070d1f8e03a4863bd87a25f57341
João Dossena <joaopdossena@gmail.com>
2019-12-09 18:09:29 +0000

    TP7 halfway done

--- TP7/Tests/dicionario.cpp
+++ TP7/Tests/dicionario.cpp
@@ -6,36 +6,102 @@
 
 using namespace std;
 
+
+
 BST<PalavraSignificado> Dicionario::getPalavras() const {
 	return palavras;
 }
 
-//a alterar
 bool PalavraSignificado::operator < (const PalavraSignificado &ps1) const {
-     return true;
+	bool result = false;
+	if(this->palavra < ps1.palavra)
+     result = true;
+ 	return result;
 }
 
-//a alterar
+bool PalavraSignificado::operator==(const PalavraSignificado &ps1) const
+{
+	bool result = false;
+	if(this->palavra == ps1.palavra && this->significado == ps1.significado)
+		result = true;
+	return result;
+}
+
+ostream& operator <<(ostream &output, PalavraSignificado &ps)
+{
+	output << ps.palavra << endl << ps.significado << endl;
+	return output;
+}
+
 void Dicionario::lerDicionario(ifstream &fich)
 {
-     return;
+	string palavra, significado;
+	while(!fich.eof())
+	{
+		getline(fich, palavra);
+		getline(fich, significado);
+
+		this->palavras.insert(PalavraSignificado(palavra, significado));     
+	}
+
+	return;
 }
 
-//a alterar
 string Dicionario::consulta(string palavra) const
 {
-    return "";
+	PalavraSignificado p1(palavra, "");
+	PalavraSignificado px = this->palavras.find(p1);
+	PalavraSignificado pNotFound("", "");
+
+	if(px == pNotFound)
+	{
+		BSTItrIn<PalavraSignificado> it(this->palavras);
+		string palAntes = "", sigAntes = "", palApos = "", sigApos = "";
+
+		while(!it.isAtEnd() && it.retrieve() < p1)
+		{
+			palAntes = it.retrieve().getPalavra();
+			sigAntes = it.retrieve().getSignificado();
+			it.advance();
+		}
+
+		if(!it.isAtEnd())
+		{
+			palApos = it.retrieve().getPalavra();
+			sigApos = it.retrieve().getSignificado();
+		}
+		throw PalavraNaoExiste(palAntes, sigAntes, palApos, sigApos);
+	}
+	else
+		return px.getSignificado();
 }
 
 //a alterar
 bool Dicionario::corrige(string palavra, string significado)
 {
-    return true;
+	bool jaExistia = false;
+	
+	BSTItrIn<PalavraSignificado> it(this->palavras);
+	PalavraSignificado pal = this->palavras.find(PalavraSignificado(palavra, ""));
+
+	if(pal == PalavraSignificado("", ""))
+	{
+		this->palavras.insert(PalavraSignificado(palavra, significado));
+	}
+	else
+	{
+		this->palavras.remove(PalavraSignificado(palavra, ""));
+		pal.setSignificado(significado);
+		this->palavras.insert(pal);
+		jaExistia = true;
+	}
+
+    return jaExistia;
 }
 
-//a alterar
 void Dicionario::imprime() const
 {
+	this->palavras.printTree();
     return;
 }
 
--- TP7/Tests/dicionario.h
+++ TP7/Tests/dicionario.h
@@ -14,6 +14,8 @@ public:
        string getSignificado() const { return significado; }
        void setSignificado(string sig) { significado=sig; }
        bool operator < (const PalavraSignificado &ps1) const;
+       bool operator==(const PalavraSignificado &ps1) const;
+       friend ostream& operator<<(ostream &output, PalavraSignificado &ps);
 };
 
 class Dicionario
@@ -26,16 +28,29 @@ public:
       string consulta(string palavra) const;
       bool corrige(string palavra, string significado);
       void imprime() const;
+
 };
 
-// a alterar
+
 class PalavraNaoExiste
 {
+private:
+      string palavraAntes;
+      string significadoAntes;
+      string palavraApos;
+      string significadoApos;
 public:
-	string getPalavraAntes() const { return ""; }
-	string getSignificadoAntes() const { return ""; }
-	string getPalavraApos() const { return ""; }
-	string getSignificadoApos() const { return ""; }
+      PalavraNaoExiste(string palAntes, string sigAntes, string palApos, string sigApos)
+      {
+            palavraAntes = palAntes;
+            significadoAntes = sigAntes;
+            palavraApos = palApos;
+            significadoApos = sigApos;
+      }
+	string getPalavraAntes() const { return this->palavraAntes; }
+	string getSignificadoAntes() const { return this->significadoAntes; }
+	string getPalavraApos() const { return this->palavraApos; }
+	string getSignificadoApos() const { return this->significadoApos; }
 };
 
 
